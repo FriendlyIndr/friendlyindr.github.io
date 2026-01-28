2c7874a0ec40b001d74ca308a07c22336ac6acf9
FriendlyIndr <abhyudaya.tiwari003@gmail.com>
2026-01-28 20:47:10 +0530

    Implement dynamic project listing and update HTML structure for projects section

--- script.js
+++ script.js
@@ -19,4 +19,41 @@ function showSection(sectionClass) {
     });
 }
 
+const projects = [
+    {
+        name: 'Resume Builder',
+        link: 'https://resume-builder-one-omega.vercel.app/',
+        gitHub: 'https://github.com/FriendlyIndr/ResumeBuilder',
+        techStack: 'JavaScript, Java, Spring Boot',
+        description: 'Resume Builder enables users to generate a PDF resume with the help of simple inputs. The only thing users have to do is insert their info and the app does the rest for them.',
+    },
+    {
+        name: 'Mas',
+        gitHub: 'https://github.com/FriendlyIndr/Mas',
+        techStack: 'JavaScript, React, Express, Node, PostgreSQL, Sequelize, Zod',
+        description: 'A simple weekly planner.',
+    }
+];
+
+function showProjects() {
+    const projectsList = document.getElementById('projects-list');
+
+    for (const project of projects) {
+        const projectDiv = document.createElement('div');
+        projectDiv.className = 'project-card';
+        projectDiv.innerHTML = `
+            <a href=${project.link ? project.link : project.gitHub} target="_blank" style="text-decoration: none;">
+                <h3>${project.name}</h3>
+            </a>
+            <p>Tech Stack: ${project.techStack}</p>
+            <p>${project.description}</p>
+            <a href=${project.gitHub} target="_blank" class="button-link">GitHub</a>
+            ${project.link ? `<a href=${project.link} target="_blank" class="button-link">Demo</a>` : ''}
+        `;
+
+        projectsList.appendChild(projectDiv);
+    }
+}
+
 showSection('projects');
+showProjects();
