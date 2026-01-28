a9cb5e4630d4e76594c853e4fc2f605155f1d08e
FriendlyIndr <abhyudaya.tiwari003@gmail.com>
2026-01-28 13:52:23 +0530

    Refactor index.html structure and update styles for improved layout and design

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ function showSection(sectionClass) {
     const buttons = document.querySelectorAll('button');
     buttons.forEach(button => {
         if (button.id === sectionClass+ '-section-btn') {
-            button.style.backgroundColor = '#222';
+            button.style.backgroundColor = '#e22a25';
+            button.style.color = '#fff';
         } else {
             button.style.backgroundColor = 'transparent';
+            button.style.color = '#000';
         }
     });
     const sections = document.querySelectorAll('.section-block');
