90f5d122bec7252d7c8a2f45dbc9a5c28e639b20
FriendlyIndr <abhyudaya.tiwari003@gmail.com>
2026-03-01 15:10:18 +0530

    Responsive navigation menu added

--- script.js
+++ script.js
@@ -38,4 +38,14 @@ for(const section of ['projects', 'skills', 'education', 'contact']) {
         .addEventListener('click', () => showSection(`${section}`));
 }
 
+document.querySelector('.mobile-menu')
+    .addEventListener('click', () => {
+        const navButtons = document.querySelector('.nav-buttons');
+        if (navButtons.style.display === 'flex') {
+            navButtons.style.display = 'none';
+        } else {
+            navButtons.style.display = 'flex';
+        }
+    });
+
 showProjects();
